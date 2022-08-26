21367ea95ccca53c01b9509e07d77312b64c8cc3
cbh <2491221443@qq.com>
2022-08-26 09:57:47 +0800

    修改application

--- src/main/java/com/sys/yms/YmsApplication.java
+++ src/main/java/com/sys/yms/YmsApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.PropertySource;
 public class YmsApplication {
 
     public static void main(String[] args) {
-        System.out.println("lllll");
         SpringApplication.run(YmsApplication.class, args);
     }
 
